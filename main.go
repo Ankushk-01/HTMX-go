e6c9e17c233e9b04e225c8573f4efa1293220f03
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-06-22 23:10:32 +0530

    added the handler method in go lang

--- main.go
+++ main.go
@@ -2,7 +2,20 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 )
-func main()  {
-	fmt.Println("Hello World");
+
+func main() {
+	fmt.Println("Hello World")
+
+	greeting := func (w http.ResponseWriter, r *http.Request) {
+		methodType := r.Method
+		io.WriteString(w,"Hello World");
+		io.WriteString(w,methodType)
+	}
+
+	http.HandleFunc("/",greeting);
+	log.Fatal(http.ListenAndServe(":8808", nil))
 }
